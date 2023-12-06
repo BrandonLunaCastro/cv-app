36df22b9c431d77c5a6e0a7e91fd741327e3ae09
Brandon luna <blunacastro2003@gmail.com>
2023-12-06 11:42:48 -0300

    feat: add states

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,63 @@
 import UserInformation from "./UserInformation";
 import { Resume } from "./Resume";
-
+import { useState } from "react";
+import { v1 as uuid } from "uuid";
 const Container = () => {
+  const [dataInfo, setDataInfo] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    telephone: "",
+    location: "",
+    social: "",
+    description: "",
+  });
+  const [educationalValues, setEducational] = useState([
+    {
+      school: "",
+      title: "",
+      dateStart: "",
+      dateEnd: "",
+      id: uuid(),
+    },
+  ]);
+  const [professionalValues, setProfessional] = useState([
+    {
+      company: "",
+      position: "",
+      responsibilities: "",
+      dateStartWork: "",
+      dateEndWork: "",
+      id: uuid(),
+    },
+  ]);
+
+  const receiveDataUser = (input, value) => {
+    setDataInfo({ ...dataInfo, [input]: value });
+  };
+
+  const receiveData = (input, value, id) => {
+    const filtered = educationalValues.filter(obj => obj.id === id).map(el => {
+      return setEducational()
+    })
+    console.log(filtered)
+  };
+  const addMoreContent = (value) => {
+    /*     if (value === "education") {
+
+    } */
+  };
+
   return (
     <section className="container">
-      <UserInformation />
+      <UserInformation
+        dataInfo={dataInfo}
+        educationalValues={educationalValues}
+        professionalValues={professionalValues}
+        receiveDataUser={receiveDataUser}
+        receiveData={receiveData}
+        addMoreContent={addMoreContent}
+      />
       <Resume />
     </section>
   );
