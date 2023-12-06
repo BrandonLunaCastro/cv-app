1b7a7cdb8fafb624845604f5090fcb9098ad1e4b
Brandon luna <blunacastro2003@gmail.com>
2023-12-06 18:08:54 -0300

    feat: finish structure of component

--- src/components/Professional.jsx
+++ src/components/Professional.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from "react";
+import { InputForm } from "./InputForm";
+import { Description } from "./Description";
 
-export default function Professional() {
+export default function Professional({ value, receiveData }) {
+  const handleChange = ({ target }) => {
+    receiveData(target.name, target.value, value.id, "profession");
+  };
   return (
-    <div>Professional</div>
-  )
+    <div className="professional">
+      <InputForm
+        onFunction={handleChange}
+        text={"Company name"}
+        name={"company"}
+        value={value.company}
+      />
+      <InputForm
+        onFunction={handleChange}
+        text={"Position title"}
+        name={"position"}
+        value={value.position}
+        />
+      <Description
+        onFunction={handleChange}
+        text={"Main responsibilities"}
+        name={"responsibilities"}
+        value={value.responsibilities}
+        />
+      <InputForm
+        onFunction={handleChange}
+        text={"Date we star work"}
+        type={"date"}
+        name={"dateStartWork"}
+        value={value.dateStartWork}
+      />
+      <InputForm
+        onFunction={handleChange}
+        text={"Date we end work"}
+        type={"date"}
+        name={"dateEndWork"}
+        value={value.dateEndWork}
+      />
+    </div>
+  );
 }
