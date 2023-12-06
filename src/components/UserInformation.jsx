f691fff4b62e8ba66ef1b3a7ab375ddfdec5ee75
Brandon luna <blunacastro2003@gmail.com>
2023-12-06 11:45:13 -0300

    feat: create component User information

--- src/components/UserInformation.jsx
+++ src/components/UserInformation.jsx
@@ -1,9 +1,78 @@
-const UserInformation = () => {
-    return (
-        <article>
-            <p>Aca va a ir la info</p>
-        </article>
-    )
-}
+import { useState } from "react";
+import Education from "./Education";
+import GeneralInformation from "./GeneralInformation";
+import Professional from "./Professional";
 
-export default UserInformation
+const UserInformation = ({
+  dataInfo,
+  educationalValues,
+  professionalValues,
+  receiveDataUser,
+  receiveData,
+  addMoreContent,
+}) => {
+  const [show, setShow] = useState(0);
+  const handleClick = (val) => {
+    if (val === show) {
+      return setShow(-1);
+    }
+    setShow(val);
+  };
+  const handleAdd = () => {
+    addMoreContent;
+  };
+
+  return (
+    <section className="content">
+      <article className="content__editable">
+        <div>
+          <p>
+            <b>GeneralInformation</b>
+          </p>
+          <button onClick={() => handleClick(0)}>+</button>
+        </div>
+        {show === 0 && (
+          <GeneralInformation
+            dataInfo={dataInfo}
+            receiveDataUser={receiveDataUser}
+          />
+        )}
+      </article>
+      <article className="content__editable">
+        <div>
+          <p>
+            <b>Educational</b>
+          </p>
+          <button onClick={() => handleClick(1)}>+</button>
+        </div>
+        {show === 1 && (
+          <>
+            {educationalValues.map((value) => {
+              return (
+                <Education
+                  values={value}
+                  receiveData={receiveData}
+                  key={value.uuid}
+                />
+              );
+            })}
+            <button onClick={handleAdd}>Add more</button>
+          </>
+        )}
+      </article>
+      <article className="content__editable">
+        <div>
+          <p>
+            <b>ProfessionalExperience</b>
+          </p>
+          <button onClick={() => handleClick(2)}>+</button>
+        </div>
+        {show === 2 && (
+          <Professional data={professionalValues} receiveData={receiveData} />
+        )}
+      </article>
+    </section>
+  );
+};
+
+export default UserInformation;
