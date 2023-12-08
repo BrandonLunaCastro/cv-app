a553574bab87999482ae605667515e0813c09a30
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-12-08 20:10:28 -0300

    fix: comment some code

--- src/components/UserInformation.jsx
+++ src/components/UserInformation.jsx
@@ -11,7 +11,7 @@ const UserInformation = ({
   receiveData,
   addMoreContent,
   deleteContent,
-  handleInputState
+  /* handleInputState */
 }) => {
   const [show, setShow] = useState(0);
   const handleClick = (val) => {
@@ -98,8 +98,8 @@ const UserInformation = ({
                   <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
                 </svg>
               </button>
-              <button className="edit" onClick={() => handleInputState("edit", "education")}>Edit</button>
-              <button className="safe" onClick={() => handleInputState("safe", "education")}>Safe</button>
+{/*           <button className="edit" onClick={() => handleInputState("edit", "education")}>Edit</button>
+              <button className="safe" onClick={() => handleInputState("safe", "education")}>Safe</button> */}
             </article>
           </>
         )}
