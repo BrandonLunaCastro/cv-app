273720339a9e3ea7e1d630a7089a3d717cfe8a53
Brandon luna <blunacastro2003@gmail.com>
2023-12-09 19:44:40 -0300

    fix: des comment buttons safe and edit

--- src/components/UserInformation.jsx
+++ src/components/UserInformation.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Education from "./Education";
 import GeneralInformation from "./GeneralInformation";
 import Professional from "./Professional";
+import Button from "./Button";
 
 const UserInformation = ({
   dataInfo,
@@ -11,7 +12,7 @@ const UserInformation = ({
   receiveData,
   addMoreContent,
   deleteContent,
-  /* handleInputState */
+  handleInputState
 }) => {
   const [show, setShow] = useState(0);
   const handleClick = (val) => {
@@ -78,12 +79,13 @@ const UserInformation = ({
                     >
                       <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
                     </svg>
-                  </button>
-                  <Education
-                    values={value}
-                    receiveData={receiveData}
-                    key={value.id}
-                  />
+                  </button>                 
+                    <Education
+                      values={value}
+                      receiveData={receiveData}
+                      key={value.id}
+                    />
+                  
                 </div>
               );
             })}
@@ -98,8 +100,8 @@ const UserInformation = ({
                   <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
                 </svg>
               </button>
-{/*           <button className="edit" onClick={() => handleInputState("edit", "education")}>Edit</button>
-              <button className="safe" onClick={() => handleInputState("safe", "education")}>Safe</button> */}
+              <button className="edit" onClick={() => handleInputState("edit", "education")}>Edit</button>
+              <button className="safe" onClick={() => handleInputState("safe", "education")}>Safe</button>
             </article>
           </>
         )}
