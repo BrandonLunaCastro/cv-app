40afe44a5f954987cf5f673bb2849cf2395e478e
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-12-09 20:55:41 -0300

    feat: add handle change function

--- src/components/GeneralInformation.jsx
+++ src/components/GeneralInformation.jsx
@@ -1,66 +1,85 @@
 import { Description } from "./Description";
 import { InputForm } from "./InputForm";
-import Button from "./Button"
+import Button from "./Button";
 /* import { formData } from "../helpers/formData"; */
 
 export default function GeneralInformation({ dataInfo, receiveDataUser }) {
-  const handleSubmit = (event) => {  
-    event.preventDefault();
-    console.log(event.target.closest())  
-
+  const handleClick = (e) => {
+    const className = e.target.className;
+    if (className === "edit") {
+      receiveDataUser(false);
+    }
+    if (className === "safe") {
+      receiveDataUser(true);
+    }
+  };
+  const handleChange = ({target}) => {
+    receiveDataUser(target.name, target.value)
   }
+
   return (
-    <form onSubmit={handleSubmit}>
+    <div>
       <Picture />
-      <InputForm
-        name={"name"}
-        text={"Name"}
-        state={dataInfo.stateInputs}
-        />
+      <InputForm name={"name"} text={"Name"} state={dataInfo.stateInputs} />
       <InputForm
         name={"surname"}
         text={"Surname"}
+        onFunction={handleChange}
         state={dataInfo.stateInputs}
       />
       <InputForm
         name={"email"}
         text={"Email"}
         type={"email"}
+        onFunction={handleChange}
         state={dataInfo.stateInputs}
       />
       <InputForm
         name={"telephone"}
         text={"Telephone"}
+        onFunction={handleChange}
         type={"tel"}
         state={dataInfo.stateInputs}
       />
       <InputForm
         name={"location"}
         text={"Location"}
+        onFunction={handleChange}
         state={dataInfo.stateInputs}
       />
       <InputForm
         name={"social"}
         text={"Social Media"}
+        onFunction={handleChange}
         state={dataInfo.stateInputs}
       />
-      <Description 
+      <Description
         name={"description"}
         text={"Description"}
+        onFunction={handleChange}
         state={dataInfo.stateInputs}
       />
-      <Button type={"button"} text={"Safe"}/>
-      <Button type={"button"} text={"Edit"} />
-    </form>
+      <Button
+        type={"button"}
+        text={"Safe"}
+        onFunction={handleClick}
+        className={"safe"}
+      />
+      <Button
+        type={"button"}
+        text={"Edit"}
+        onFunction={handleClick}
+        className={"edit"}
+      />
+    </div>
   );
 }
-
 
 const Picture = () => {
   return (
     <div>
       <label>picture</label>
-      <button className="import" type="button" >
+      <button className="import" type="button">
         <span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -73,8 +92,8 @@ const Picture = () => {
         </span>
       </button>
       <dialog className="dialog__img">
-          <input type="file"></input>
-      </dialog> 
+        <input type="file"></input>
+      </dialog>
     </div>
   );
 };
