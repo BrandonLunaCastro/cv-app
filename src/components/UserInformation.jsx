d636bff75822fd9ccf8f1b2a0eb73ab55ee1a177
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-12-12 09:11:22 -0300

    feat: add handle function to edit and save data

--- src/components/UserInformation.jsx
+++ src/components/UserInformation.jsx
@@ -12,7 +12,7 @@ const UserInformation = ({
   receiveData,
   addMoreContent,
   deleteContent,
-  handleInputState
+  changeState,
 }) => {
   const [show, setShow] = useState(0);
   const handleClick = (val) => {
@@ -21,6 +21,34 @@ const UserInformation = ({
     }
     setShow(val);
   };
+
+  const SubmitEducation = (e) => {
+    e.preventDefault();
+    changeState("education", true);
+  };
+  const EditEducation = () => {
+    changeState("education", false);
+  };
+
+  const submitProfession = (e) => {
+    e.preventDefault();
+    changeState("profession", true)
+  }
+
+  const EditProfession = () => {
+    changeState("profession", false)
+  }
+
+  const addSvg = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="16"
+      width="14"
+      viewBox="0 0 448 512"
+    >
+      <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+    </svg>
+  );
 
   /*   const up = (
     <svg
@@ -55,6 +83,7 @@ const UserInformation = ({
           <GeneralInformation
             dataInfo={dataInfo}
             receiveDataUser={receiveDataUser}
+            changeState={changeState}
           />
         )}
       </article>
@@ -66,7 +95,7 @@ const UserInformation = ({
           <button onClick={() => handleClick(1)}>+</button>
         </div>
         {show === 1 && (
-          <>
+          <form onSubmit={SubmitEducation}>
             {educationalValues.map((value) => {
               return (
                 <div key={value.id}>
@@ -79,30 +108,28 @@ const UserInformation = ({
                     >
                       <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
                     </svg>
-                  </button>                 
-                    <Education
-                      values={value}
-                      receiveData={receiveData}
-                      key={value.id}
-                    />                 
+                  </button>
+                  <Education
+                    values={value}
+                    receiveData={receiveData}
+                    key={value.id}
+                  />
                 </div>
               );
             })}
             <article>
-              <button onClick={() => addMoreContent("education")}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="16"
-                  width="14"
-                  viewBox="0 0 448 512"
-                >
-                  <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-                </svg>
-              </button>
-              <button className="edit" onClick={() => handleInputState("edit", "education")}>Edit</button>
-              <button className="safe" onClick={() => handleInputState("safe", "education")}>Safe</button>
+              <Button
+                onFunction={() => addMoreContent("education")}
+                element={addSvg}
+              />
+              <Button type={"submit"} text={"safe"} />
+              <Button
+                type={"button"}
+                text={"edit"}
+                onFunction={EditEducation}
+              />
             </article>
-          </>
+          </form>
         )}
       </article>
       <article className="content__editable">
@@ -113,7 +140,7 @@ const UserInformation = ({
           <button onClick={() => handleClick(2)}>+</button>
         </div>
         {show === 2 && (
-          <>
+          <form onSubmit={submitProfession}>
             {professionalValues.map((value) => {
               return (
                 <div key={value.id}>
@@ -136,24 +163,18 @@ const UserInformation = ({
               );
             })}
             <article>
-              <button onClick={() => addMoreContent("profession")}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="16"
-                  width="14"
-                  viewBox="0 0 448 512"
-                >
-                  <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-                </svg>
-              </button>
-              <button id="edit" onClick={() => handleInputState("edit", "profession")}>
-                Edit
-              </button>
-              <button id="safe" onClick={() => handleInputState("safe", "profession")}>
-                Safe
-              </button>
+            <Button
+                onFunction={() => addMoreContent("profession")}
+                element={addSvg}
+              />
+              <Button type={"submit"} text={"safe"} />
+              <Button
+                type={"button"}
+                text={"edit"}
+                onFunction={EditProfession}
+              />
             </article>
-          </>
+          </form>
         )}
       </article>
     </section>
