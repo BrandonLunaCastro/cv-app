6fbade027cc0398cc0da671bde06aca195730a2b
Brandon luna <blunacastro2003@gmail.com>
2023-12-12 11:52:37 -0300

    feat: import ShowGeneral data

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,26 +1,23 @@
 import { verifyState } from "../helpers/verifyState";
 import EducationExperience from "./EducationExperience";
 import ProfessionExperience from "./ProfessionExperience";
+import ShowGeneral from "./showGeneral";
+
 
 export const Resume = ({ dataInfo, educationalValues, professionalValues }) => {
   return (
     <section className="resume">
       <section className="left">
-        <img alt=""></img>
-        <p className="description">Description</p>
-        <p>{dataInfo.description}</p>
-        <p className="contact">Contact</p>
-        <ul>
-          <li><address>{dataInfo.email}</address></li>
-          <li><address>{dataInfo.telephone}</address> </li>
-          <li><address>{dataInfo.social}</address></li>
-          <li><address>{dataInfo.location}</address></li>
-        </ul>
+          {
+            dataInfo.stateInputs && 
+            <ShowGeneral dataInfo={dataInfo}/>
+          }
       </section>
       <section className="right">
-        <h1>
-          {dataInfo.name} {dataInfo.surname}
-        </h1>
+          { dataInfo.stateInputs &&
+            <h1>{dataInfo.name} {dataInfo.surname}</h1>
+          }
+        
         <p className="education">Education</p>
         { verifyState(educationalValues) &&
           <EducationExperience values={educationalValues}/>
