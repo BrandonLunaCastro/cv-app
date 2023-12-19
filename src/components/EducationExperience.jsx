054ffa141ce77457411af7aca410607c1ea61e83
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-12-18 22:15:14 -0300

    feat: refactor education experience

--- src/components/EducationExperience.jsx
+++ src/components/EducationExperience.jsx
@@ -1,16 +1,19 @@
+import "../styles/EducationExperience.css";
 function EducationExperience({ values }) {
   return (
     <>
       {values
         .filter((el) => el.stateInputs)
         .map((val) => {
+          const dateStart = new Date(val.dateStart) 
+          const dateEnd = new Date(val.dateEnd)
           return (
-            <div key={val.id}>
-              <p>{val.school}</p>
-              <b>
-                {val.dateStart} - {val.dateEnd}
+            <div key={val.id} className="education__experience">
+              <p className="school">{val.school}</p>
+              <b className="date__study">
+                {dateStart.getFullYear()} - {dateEnd.getFullYear()}
               </b>
-              <small>{val.title}</small>
+              <p className="tuition">{val.title}</p>
             </div>
           );
         })}
