4227d07e29b4a33b84d2f46e01d9bb20fb629bb9
Brandon luna <blunacastro2003@gmail.com>
2023-12-21 15:12:11 -0300

    feat: add validation to date inputs

--- src/components/EducationExperience.jsx
+++ src/components/EducationExperience.jsx
@@ -5,8 +5,8 @@ function EducationExperience({ values }) {
       {values
         .filter((el) => el.stateInputs)
         .map((val) => {
-          const dateStart = new Date(val.dateStart) 
-          const dateEnd = new Date(val.dateEnd)
+          const dateStart = !val.dateStart ? new Date() : new Date(val.dateStart);
+          const dateEnd =  !val.dateEnd ? new Date() : new Date(val.dateEnd);
           return (
             <div key={val.id} className="education__experience">
               <p className="school">{val.school}</p>
